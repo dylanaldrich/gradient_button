628a23e77eb668796a0074b8ef9045a96ae31f5d
dylanaldrich <aldrich.dylan@gmail.com>
2021-08-16 10:59:58 -0700

    Adds random background color solid

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-console.log("Hello world!");
+function changeColor() {
+    const hexNums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 
-$('#my-btn').on('click', function() {
-    console.log($('body').css("background-color"));
-    if($('body').css("background-color") === "blue") {
-        $('body').css("background-color", "red");
-    } else {
-        $('body').css("background-color", "blue");
+    let hexCode = "";
+
+    for(let i = 0; i < 6; i++) {
+        let randomIndex = Math.floor(Math.random() * hexNums.length);
+        hexCode += hexNums[randomIndex];
     }
-});
+
+    document.body.style.backgroundColor = "#" + hexCode;
+    document.getElementById("hex-code").innerHTML = hexCode;
+}
